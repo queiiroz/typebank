79e3ac75db73bd9fe80fb3642cae8b8d57ecca4b
Gleidson Queiroz <gldsnqueiroz2@gmail.com>
2024-03-13 20:03:32 -0300

    refatorando com ts

---
+++ js/typebank.ts
@@ -0,0 +1,57 @@
+let saldo = 3000;
+
+const elementoSaldo = document.querySelector(
+  ".block-saldo .valor"
+) as HTMLElement;
+if (elementoSaldo != null) {
+  elementoSaldo.textContent = saldo.toString();
+}
+
+const elementoFormulario = document.querySelector(
+  ".block-nova-transacao form"
+) as HTMLFormElement;
+elementoFormulario.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  if (!elementoFormulario.checkValidity()) {
+    alert("Por favor, preencha todos o campos da transação");
+    return;
+  }
+
+  const inputTipoTransacao = elementoFormulario.querySelector(
+    "#tipoTransacao"
+  ) as HTMLSelectElement;
+  const inputValor = elementoFormulario.querySelector(
+    "#valor"
+  ) as HTMLInputElement;
+  const inputData = elementoFormulario.querySelector(
+    "#data"
+  ) as HTMLInputElement;
+
+  let tipoTransacao: string = inputTipoTransacao.value;
+  let valor: number = inputValor.valueAsNumber;
+  let data: Date = new Date(inputData.value);
+
+  if (tipoTransacao == "Depósito") {
+    saldo += valor;
+  } else if (
+    tipoTransacao == "Transferência" ||
+    tipoTransacao == "Pagamento de Boleto"
+  ) {
+    saldo -= valor;
+  } else {
+    alert("Tipo de Transação é inválido!");
+    return;
+  }
+
+  elementoSaldo.textContent = saldo.toString();
+
+  const novaTransacao = {
+    tipoTransacao: tipoTransacao,
+    valor: valor,
+    data: data,
+  };
+
+  console.log(novaTransacao);
+  elementoFormulario.reset();
+});
